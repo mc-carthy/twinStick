83fbd0072aa6ce9fee7b9ab5a6da0b7897d4cdb4
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-22 17:06:34 +0100

    Added enemies

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public int health = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Debug.Log ("Hit " + collision.gameObject.name);
+
+		if (collision.gameObject.tag == "Projectile") {
+			LaserBehaviour laser = collision.gameObject.GetComponent<LaserBehaviour> ();
+			health -= laser.damage;
+			Destroy (collision.gameObject);
+		}
+
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsPlayer : MonoBehaviour {
+
+	private Transform player;
+	public float speed = 2.0f;
+
+	void Start () {
+		player = FindObjectOfType<PlayerBehaviour> ().transform;
+	}
+	
+	void Update () {
+		Vector3 delta = player.position - transform.position;
+		float moveSpeed = speed * Time.deltaTime;
+		transform.position = transform.position + (delta * moveSpeed);
+	}
+}
